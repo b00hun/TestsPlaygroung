4f22dc0f78ae30f624b0d6098d26c13855cca7ba
Bartosz Sadowski <bshadowsky@gmail.com>
2023-01-05 19:58:05 +0100

    first tests

---
+++ MyProject.Test/BmiDeterminatorTests.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Xunit;
+
+namespace MyProject.Test
+{
+    public class BmiDeterminatorTests
+    {
+        [Theory]
+        [InlineData(10.0)]
+        [InlineData(15.2)]
+        [InlineData(18.0)]
+        public void DetermineBmi_ForBmiUnder18_5_ReturnsUnderweight(double bmi)
+        {
+            // arrange
+
+            BmiDeterminator bmiDeterminator = new BmiDeterminator();
+            
+
+            // act
+
+            BmiClassification result = bmiDeterminator.DetermineBmi(bmi);
+
+            // assert
+            Assert.Equal(BmiClassification.Underweight,result);
+        }
+
+
+        
+    }
+}
--- MyProject.Test/UnitTest1.cs
+++ MyProject.Test/UnitTest1.cs
@@ -1,31 +1,29 @@
-using System;
-using System.Net.Http;
 using System.Text;
 using Xunit;
 
-namespace MyProject.Tests
+namespace MyProject.Test
 {
     public class StringBuilderTests
     {
         [Fact]
-        public void Append_ForTwoStrings_ReturnsConcatenatedString()
+        public void Append_ForTwoStrings_ReturnsConcatenatedTwoStrings()
         {
-            // arrange
+            //arrange
 
-            StringBuilder sb = new StringBuilder();
+            StringBuilder sb = new();
 
+           
             // act
+
             sb.Append("test")
                 .Append("test2");
 
             string result = sb.ToString();
 
-
+           
             // assert
 
             Assert.Equal("testtest2", result);
-
         }
-
     }
 }
