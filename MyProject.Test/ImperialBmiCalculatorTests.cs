02ec104f7333d1fb882d57c62bb77888d3e9d28c
Bartosz Sadowski <bshadowsky@gmail.com>
2023-01-07 15:31:32 +0100

    bmiCalculatorTestAdded

---
+++ MyProject.Test/ImperialBmiCalculatorTests.cs
@@ -0,0 +1,55 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Xunit;
+
+namespace MyProject.Test
+{
+    public class ImperialBmiCalculatorTests
+    {
+        [Theory]
+        [InlineData(180,75,22.5)]
+        [InlineData(185, 81, 19.82)]
+        [InlineData(200, 70, 28.69)]
+        [InlineData(215, 82, 22.48)]
+
+
+        public void CalculateBmi_ForGivenWeightAndHeight_ReturnsCorrectBmi(double weight,double height, double bmiResult)
+        {
+            //arrange
+            ImperialBmiCalculator calculator = new();
+
+            //act
+            double result = calculator.CalculateBmi(weight, height);
+
+            //assert
+            Assert.Equal(bmiResult, result);
+        }
+
+
+        [Theory]
+        [InlineData(0, 190)]
+        [InlineData(-5, 190)]
+        [InlineData(-11, 190)]
+        [InlineData(90, 0)]
+        [InlineData(90, -16)]
+        [InlineData(0, -180)]
+        [InlineData(0, 0)]
+        public void CalculateBmi_ForInvalidArguments_ThrowsArgumentException(double weight, double height)
+        {
+            //arrange
+            ImperialBmiCalculator calculator = new();
+
+            //act
+
+            Action action = () => calculator.CalculateBmi(weight, height);
+
+            //assert
+
+            Assert.Throws<ArgumentException>(action);
+
+        }
+    }
+}
---
+++ MyProject.Test/MetricBmiCalculatorTests.cs
@@ -0,0 +1,56 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Xunit;
+
+namespace MyProject.Test
+{
+    public class MetricBmiCalculatorTests
+    {
+        [Theory]
+        [InlineData(100,170,34.6)]
+        [InlineData(57,170,19.72)]
+        [InlineData(70,170,24.22)]
+        [InlineData(90,190,24.93)]
+        public void CalculateBmi_ForGivenWeightAndHeight_ReturnsCorrectBmi(double weight, double height, double bmiResult)
+        {
+
+            //arrange
+
+            MetricBmiCalculator calculator = new();
+
+            //act
+
+            double result = calculator.CalculateBmi(weight, height);
+
+            //assert
+
+            Assert.Equal(bmiResult, result);
+        }
+
+        [Theory]
+        [InlineData(0,190)]
+        [InlineData(-5, 190)]
+        [InlineData(-11, 190)]
+        [InlineData(90, 0)]
+        [InlineData(90, -16)]
+        [InlineData(0, -180)]
+        [InlineData(0,0)]
+        public void CalculateBmi_ForInvalidArguments_ThrowsArgumentException(double weight,double height)
+        {
+            //arrange
+            MetricBmiCalculator calculator = new();
+
+            //act
+            
+            Action action = () => calculator.CalculateBmi(weight,height);
+
+            //assert
+            
+            Assert.Throws<ArgumentException>(action);
+
+        }
+    }
+}
